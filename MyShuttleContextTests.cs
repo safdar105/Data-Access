1389ac23c4de26f89577222427c95af9756a9f87
safdar105 <safdarhussain@live.com>
2024-06-05 13:11:14 -0400

    Add files via upload

---
+++ MyShuttleContextTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using MyShuttle.Data;
+
+namespace MyShuttle.IntegrationTests
+{
+    public class MyShuttleContextTests
+    {
+        [Fact]
+        public async Task Database_CreatedSuccessfully()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder()
+                .UseInMemoryDatabase("MyShuttleTestDb");
+            using var context = new MyShuttleContext(optionsBuilder.Options);
+
+            var databaseCreated = await context.Database.EnsureCreatedAsync();
+            Assert.True(databaseCreated);
+        }
+
+        [Fact]
+        public async Task Database_DeletedSuccessfully()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder()
+                .UseInMemoryDatabase("MyShuttleTestDb");
+            using var context = new MyShuttleContext(optionsBuilder.Options);
+
+            _ = await context.Database.EnsureCreatedAsync();
+
+            var databaseDeleted = await context.Database.EnsureDeletedAsync();
+            Assert.True(databaseDeleted);
+        }
+    }
+}
